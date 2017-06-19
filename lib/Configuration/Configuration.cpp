7f85b9bae50dcad441c496cd797f5099a2a32992
Joris Rietveld <jorisrietveld@gmail.com>
2017-06-20 00:47:06 +0200

    Fixed some bugs.

--- lib/Configuration/Configuration.cpp
+++ lib/Configuration/Configuration.cpp
@@ -6,6 +6,10 @@
  */
 #include "Configuration.h"
 
+LedSettings ledSettingsObject{};
+MQTTSettings mqttSettingsObject{};
+PlantCareSettings plantCareSettingsObject{};
+
 Configuration::Configuration()
 {
     Configuration(DEFAULT_EEPROM_ADDRESS_OFFSET);
@@ -26,6 +30,10 @@ void Configuration::setup()
     EEPROM.begin(EEPROM_MEMORY_SIZE);
     delay(10);
 
+    setLedSettings( &ledSettingsObject );
+    setMQTTSettings( &mqttSettingsObject );
+    setPlantCareSettings( &plantCareSettingsObject );
+
     readSettings(this->ledSettingsAddress, this->ledSettings);
     readSettings(this->mqttSettingsAddress, this->mqttSettings);
     readSettings(this->plantCareSettingsAddress, this->plantCareSettings);
@@ -34,22 +42,22 @@ void Configuration::setup()
 void Configuration::resetToDefaults()
 {
     this->setLedSettings(
-            DEFAULT_SETTING_LED_RED,
-            DEFAULT_SETTING_LED_GREEN,
-            DEFAULT_SETTING_LED_BLUE
+            (uint8_t)DEFAULT_SETTING_LED_RED,
+            (uint8_t)DEFAULT_SETTING_LED_GREEN,
+            (uint8_t)DEFAULT_SETTING_LED_BLUE
     );
 
     this->setMQTTSettings(
-            DEFAULT_SETTING_MQTT_PING_INTERVAL,
-            DEFAULT_SETTING_MQTT_WARNING_INTERVAL,
-            DEFAULT_SETTING_MQTT_PING_INTERVAL,
-            DEFAULT_SETTING_MQTT_RESERVOIR_WARNING_THRESHOLD
+            (uint16_t)DEFAULT_SETTING_MQTT_PING_INTERVAL,
+            (uint16_t)DEFAULT_SETTING_MQTT_WARNING_INTERVAL,
+            (uint16_t)DEFAULT_SETTING_MQTT_PING_INTERVAL,
+            (uint8_t)DEFAULT_SETTING_MQTT_RESERVOIR_WARNING_THRESHOLD
     );
 
     this->setPlantCareSettings(
-            DEFAULT_SETTING_PLANT_CARE_MEASURE_INTERVAL,
-            DEFAULT_SETTING_PLANT_CARE_SLEEP_AFTER_WATER,
-            DEFAULT_SETTING_PLANT_CARE_MOISTURE_OPTIMAL
+            (uint16_t)DEFAULT_SETTING_PLANT_CARE_MEASURE_INTERVAL,
+            (uint16_t)DEFAULT_SETTING_PLANT_CARE_SLEEP_AFTER_WATER,
+            (uint8_t)DEFAULT_SETTING_PLANT_CARE_MOISTURE_OPTIMAL
     );
 }
 
@@ -74,7 +82,6 @@ void Configuration::setLedSettings(uint8_t red, uint8_t green, uint8_t blue)
     this->ledSettings->red = red;
     this->ledSettings->green = green;
     this->ledSettings->blue = blue;
-    //this->setLedSettings( &(LedSettings){red, green, blue});
 }
 
 void Configuration::setMQTTSettings(MQTTSettings* mqttSettings)
@@ -89,8 +96,6 @@ void Configuration::setMQTTSettings(uint16_t statisticPublishInterval, uint16_t 
     this->mqttSettings->resendWarningInterval = resendWarningInterval;
     this->mqttSettings->pingBrokerInterval = pingBrokerInterval;
     this->mqttSettings->publishReservoirWarningThreshold = publishReservoirWarningThreshold;
-    //setLedSettings((MQTTSettings*) {statisticPublishInterval, resendWarningInterval, pingBrokerInterval, publishReservoirWarningThreshold});
-    // ?
 }
 
 void Configuration::setPlantCareSettings(PlantCareSettings* plantCareSettings)
@@ -101,20 +106,22 @@ void Configuration::setPlantCareSettings(PlantCareSettings* plantCareSettings)
 
 void Configuration::setPlantCareSettings(uint16_t takeMeasurementInterval, uint16_t sleepAfterGivingWater, uint8_t groundMoistureOptimal)
 {
-    this->setPlantCareSettings((PlantCareSettings*) {takeMeasurementInterval, sleepAfterGivingWater, groundMoistureOptimal});
+    this->plantCareSettings->takeMeasurementInterval = takeMeasurementInterval;
+    this->plantCareSettings->sleepAfterGivingWater = sleepAfterGivingWater;
+    this->plantCareSettings->groundMoistureOptimal = groundMoistureOptimal;
 }
 
-LedSettings Configuration::getLedSettings()
+LedSettings *Configuration::getLedSettings()
 {
     return this->ledSettings;
 }
 
-MQTTSettings Configuration::getMqttSettings()
+MQTTSettings *Configuration::getMqttSettings()
 {
     return this->mqttSettings;
 }
 
-PlantCareSettings Configuration::getPlantCareSettings()
+PlantCareSettings *Configuration::getPlantCareSettings()
 {
     return this->plantCareSettings;
 }
