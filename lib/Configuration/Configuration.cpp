03b8db56f01f1364f8a31a99074886e609dfe292
Joris Rietveld <jorisrietveld@gmail.com>
2017-06-23 15:54:03 +0200

    Fixed some bugs.

--- lib/Configuration/Configuration.cpp
+++ lib/Configuration/Configuration.cpp
@@ -1,5 +1,6 @@
 /**
  * Author: Joris Rietveld <jorisrietveld@gmail.com>
+ * Author: Alwin Kroezen <alwin.kroesen@student.stenden.com>
  * Created: 19-06-2017 17:51
  * Licence: GPLv3 - General Public Licence version 3
  */
